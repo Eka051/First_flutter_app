41cecdc6b039ba268bc4c0aba293236eabc38251
Dian Eka Raharjo <dianekaraharjo@gmail.com>
2025-01-05 08:10:13 +0700

    feat: add text color and adjust font size

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,21 @@ void main() {
   runApp(MaterialApp(
     home: Scaffold(
         body: Container(
-      decoration: BoxDecoration(
+      decoration: const BoxDecoration(
         gradient: LinearGradient(
-          colors: [const Color.fromARGB(255, 8, 64, 111), Colors.blue],
+          colors: [Color.fromARGB(255, 8, 64, 111), Colors.blue],
           begin: Alignment.topLeft,
           end: Alignment.bottomRight,
         ),
       ),
       child: Center(
-        child: Text('Hello World!'),
+        child: Text(
+          'Hello World!',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 28,
+          ),
+        ),
       ),
     )),
     debugShowCheckedModeBanner: false,
