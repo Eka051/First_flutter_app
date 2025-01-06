87400a12c0e9c5c6d6874e7a47817b8560615374
Dian Eka Raharjo <dianekaraharjo@gmail.com>
2025-01-06 07:47:10 +0700

    feat: create custom widget with StyledText widget for consistent text styling

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/styled_text.dart';
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
@@ -14,13 +15,7 @@ class GradientContainer extends StatelessWidget {
         ),
       ),
       child: Center(
-        child: Text(
-          'Hello World!',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 28,
-          ),
-        ),
+        child: StyledText()
       ),
     );
   }
