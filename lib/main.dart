7751145bcf99beeeaf10ad2b28beffc6a22a963c
Dian Eka Raharjo <dianekaraharjo@gmail.com>
2025-01-06 08:50:07 +0700

    feat: update GradientContainer to accept dynamic colors for gradient

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,11 @@ import 'package:flutter_app/gradient_container.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-        body: GradientContainer() 
+      body: GradientContainer(
+        Color.fromARGB(255, 2, 10, 50),
+        Color.fromARGB(255, 7, 136, 187),
       ),
+    ),
     debugShowCheckedModeBanner: false,
   ));
 }
-
-
