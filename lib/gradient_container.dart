7e4ac4adc0a437e0bb7c092b66c80bce208c7fcf
Dian Eka Raharjo <dianekaraharjo@gmail.com>
2025-01-06 07:57:09 +0700

    feat: refactor gradient container to use variables for alignment

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,22 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/styled_text.dart';
 
+var startAlignment = Alignment.topLeft;
+var endAlignment = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
 
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [Color.fromARGB(255, 8, 64, 111), Colors.blue],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+          colors: const [Color.fromARGB(255, 8, 64, 111), Colors.blue],
+          begin: startAlignment,
+          end: endAlignment,
         ),
       ),
-      child: Center(
-        child: StyledText()
-      ),
+      child: Center(child: StyledText()),
     );
   }
 }
